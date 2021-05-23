200e3fe234ac77c04812f2092ea8aa31bfba2e22
Ibrahimhussien227 <65757724+Ibrahimhussien227@users.noreply.github.com>
2021-05-23 06:52:30 +0300

    make it better

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'django_countries',
     'rest_framework',
     'channels',
+    'favicon',
+
 ]
 
 MIDDLEWARE = [
@@ -145,6 +147,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+FAVICON_PATH = STATIC_URL + 'images/favicon.png'
 
 
 MEDIA_URL = '/img/'
